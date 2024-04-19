82416f0b3388c86246d8bf4fcf2c86e94c872fe9
Josefa-Merida16 <163026438+Josefa-Merida16@users.noreply.github.com>
2024-04-19 23:31:16 +0800

    Add project files.

---
+++ PaintingBL/BusinessLogic.cs
@@ -0,0 +1,16 @@
+﻿using PaintingModel;
+using PaintingDL;
+using System.Reflection;
+namespace PaintingBL
+{
+    public class BusinessLogic
+    {
+
+        public List<Model> GMS()
+        {
+            DataLayer dataServices = new DataLayer();
+            return dataServices.GMS();
+
+        }
+    }
+}
